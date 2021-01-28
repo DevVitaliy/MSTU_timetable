bedaa4555e0b6eb6a0260d9058c387feda802819
Vitaliy Nezgovorov <nezgovorovvy@gmail.com>
2021-01-28 18:53:32 +0300

    feat: configure tg keyboard

--- bot.py
+++ bot.py
@@ -2,15 +2,21 @@ import logging
 import settings
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 
-API_TOKEN = 'BOT TOKEN HERE'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+
 # Initialize bot and dispatcher
 bot = Bot(token=settings.TG_TOKEN)
 dp = Dispatcher(bot)
+
+
+# Configure keyboard
+button_timetable = KeyboardButton('Расписание')
+timetable_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button_timetable)
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -18,15 +24,7 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
-
-    await message.answer(message.text)
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.", reply_markup=timetable_kb)
 
 
 if __name__ == '__main__':
